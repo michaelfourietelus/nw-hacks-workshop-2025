5a5edec3577f9caecdf65bb4fcf269841ddb082b
Michael Fourie <148267978+michaelfourietelus@users.noreply.github.com>
2025-01-10 10:35:59 -0800

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -8,13 +8,13 @@ import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDnEzsTHHF3Qg0fLvPxgzChqUsGeiGtV48",
-  authDomain: "anonymous-qa.firebaseapp.com",
+  apiKey: "",
+  authDomain: ".firebaseapp.com",
   projectId: "anonymous-qa",
   storageBucket: "anonymous-qa.firebasestorage.app",
-  messagingSenderId: "849688107498",
-  appId: "1:849688107498:web:0f72a1a3c391b5dfcc3a93",
-  measurementId: "G-3G95WYCFPZ"
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
